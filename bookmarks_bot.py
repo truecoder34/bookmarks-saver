29da36e4db851f099c6243c6e974f47c38ccef3e
Vladislav <vladplotnikov34@gmail.com>
2021-11-07 15:43:40 +0300

    only remove topc left to add

--- bookmarks_bot.py
+++ bookmarks_bot.py
@@ -1,13 +1,16 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from storage_controller import check_if_id_exists
+from storage_controller import check_if_id_exists, add_chat_id_to_storage, add_topic_by_id_to_storage, add_url_by_id_and_topic_to_storage, check_if_topic_exists, list_topics_by_id, list_bookmarks_by_id_and_topic, remove_url_by_id_and_topic, remove_topic_by_id
 import os
+import re
 
 URL_REGEX = "^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$"
 STATE = None
 REPLIES = {
-    "USER_NOT_EXISTS"   : "Hello, {}! You are new to our bot. Let's start our work ... ; What action do you want to do? ",
+    "USER_NOT_EXISTS"   : "Hello, {}! You are new to our bot. Let's start our work ... ; Here is some information about bot - call > /help \n",
     "USER_EXISTS"       : "{}, welcome back! What action do you want to do?", 
-    "HELP" : "It's a bot which will store all your improtant links on web-sources. \nCreate a topic, add urls in it. It is useful when you dont have time to read . \nNo problems, save it to me and come back later !"
+    "HELP" : "It's a bot which will store all your improtant links on web-sources. \nCreate a topic, add urls in it. It is useful when you dont have time to read . \nNo problems, save it to me and come back later !",
+    "HELP_TOPIC": "To addd a topic call command > /add_topic \nTo list your existing topics call command > /list_topic \nTo remove topic (with all bookmarks in it) call > /remove_topic",
+    "HELP_BOOKMARK": "To addd a bookmark call command > /add_bookmark \nTo list your existing bookmarks in topic call command > /list_bookmark \nTo remove bookmark  call > /remove_bookmark",
 }
 
 def get_env_data_as_dict(path: str) -> dict:
@@ -15,6 +18,9 @@ def get_env_data_as_dict(path: str) -> dict:
        return dict(tuple(line.replace('\n', '').split('=')) for line
                 in f.readlines() if not line.startswith('#'))
 
+def check_url_matches(str, reg):
+    is_match = bool(re.match(reg, str))
+    return is_match
 
 
 # function to handle the /start command
@@ -22,15 +28,21 @@ def start(update, context):
     first_name = update.message.chat.first_name
     print(update.message.chat)
     print(update.message.chat.id)
+    print(update.message)
     if check_if_id_exists(update.message.chat.id):
         update.message.reply_text(REPLIES["USER_EXISTS"].format(first_name))
     else:
         update.message.reply_text(REPLIES["USER_NOT_EXISTS"].format(first_name))
+        add_chat_id_to_storage(str(update.message.chat.id))
+
 
 
 
 def help(update, context):
     update.message.reply_text(REPLIES["HELP"])
+    update.message.reply_text(REPLIES["HELP_TOPIC"])
+    update.message.reply_text(REPLIES["HELP_BOOKMARK"])
+
 
 
 def error(update, context):
@@ -43,23 +55,90 @@ def text(update, context):
 ################# CONTROLLERS CUSTOM ####################
 ####################### TOPIC ###########################
 def add_topic(update, context):
-    update.message.reply_text('add_topic')
+    try:
+        topic = context.args[0]
+        print(topic)
+        if check_if_topic_exists(str(update.message.chat.id), topic):
+            update.message.reply_text('[INFO]: Topic already exists.{}'.format(topic))
+        else:
+            update.message.reply_text('[INFO]: Topic {} dont exists. adding it ... '.format(topic))
+            add_topic_by_id_to_storage(str(update.message.chat.id), topic)
+    except (IndexError, ValueError):
+        update.message.reply_text('[ERROR] : There are no topic specified')
+
 def list_topic(update, context):
-    update.message.reply_text('list_topicd')
+    try:
+        topics = list_topics_by_id(str(update.message.chat.id))
+        for topic in topics:
+            update.message.reply_text('[RESULT]: topic - {}. To list bookmarks call '.format(topic))
+            update.message.reply_text('/list_bookmark {}'.format(topic))
+    except (IndexError, ValueError):
+        update.message.reply_text('[ERROR] : There are no topics')
+
 def remove_topic(update, context):
     update.message.reply_text('remove_topic')
 
 #########################################################
 ##################### Bookmark ##########################
 def add_bookmark(update, context):
-    update.message.reply_text('add_bookmark')
-def list_bookmark(update, context):
-    update.message.reply_text('list_bookmark')
-def remove_bookmark(update, context):
-    update.message.reply_text('remove_bookmark')
+    try:
+        topic = context.args[0]
+        url = context.args[1]
+        print(topic, " - ", url)
+        # 1 - check if topic exists
+        if check_if_topic_exists(str(update.message.chat.id), topic):
+            update.message.reply_text('[INFO]: Topic {} already exists.'.format(topic))
+        else:
+            # create topic
+            update.message.reply_text('[INFO]: Topic {} dont exists. adding it with bookmark...'.format(topic))
+            add_topic_by_id_to_storage(str(update.message.chat.id), topic)
+        
+        # 2 - check if URL correct by regex 
+        if check_url_matches(url, URL_REGEX):
+            add_url_by_id_and_topic_to_storage(str(update.message.chat.id), topic, url)
+            update.message.reply_text('[INFO]: Bookmark added to topic {}'.format(topic))
+        else:
+            update.message.reply_text('[ERROR] : Incorrect URL specified ')
 
-#########################################################
-############### READ \ WRITE JSON STORAGE ###############
+    except (IndexError, ValueError):
+        update.message.reply_text('[ERROR] : There are no topic or url specified')
+
+
+def list_bookmark(update, context):
+    try:
+        topic = context.args[0]
+    except (IndexError, ValueError):
+        update.message.reply_text('[ERROR] : There are no topic specified')
+
+    try:
+        if check_if_topic_exists(str(update.message.chat.id), topic):
+            bookmarks_in_topic = list_bookmarks_by_id_and_topic(str(update.message.chat.id), topic)
+            for url in bookmarks_in_topic:
+                update.message.reply_text('[RESULT]: - {}'.format(url))
+            
+            if len(bookmarks_in_topic) == 0:
+                update.message.reply_text('[RESULT]: TOPIC IS EMPTY ')
+
+        else:
+            update.message.reply_text('[RESULT]: Specified topic doesnot exists in storage ')
+    except (IndexError, ValueError):
+        update.message.reply_text('[ERROR] : There are no topic specified')
+
+def remove_bookmark(update, context):
+    try:
+        topic = context.args[0]
+        url = context.args[1]
+    except (IndexError, ValueError):
+        update.message.reply_text('[ERROR] : There are no topic or url specified')
+
+    try:
+        if check_if_topic_exists(str(update.message.chat.id), topic):
+           remove_url_by_id_and_topic(str(update.message.chat.id), topic, url)
+           update.message.reply_text('[RESULT]: URL WAS DELETED')
+        else:
+            update.message.reply_text('[RESULT]: NOTHING TO DELETE  ')
+    except (IndexError, ValueError):
+        update.message.reply_text('[ERROR] : ')
 
 
 
@@ -73,13 +152,13 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
     
-    dispatcher.add_handler(CommandHandler("add_topic", add_topic))
-    dispatcher.add_handler(CommandHandler("list_topic", list_topic))
-    dispatcher.add_handler(CommandHandler("remove_topic", remove_topic))
+    dispatcher.add_handler(CommandHandler("add_topic", add_topic, pass_args=True))
+    dispatcher.add_handler(CommandHandler("list_topic", list_topic, pass_args=True))
+    dispatcher.add_handler(CommandHandler("remove_topic", remove_topic, pass_args=True))
 
-    dispatcher.add_handler(CommandHandler("add_bookmark", add_bookmark))
-    dispatcher.add_handler(CommandHandler("list_bookmark", list_bookmark))
-    dispatcher.add_handler(CommandHandler("remove_bookmark", remove_bookmark))
+    dispatcher.add_handler(CommandHandler("add_bookmark", add_bookmark, pass_args=True))
+    dispatcher.add_handler(CommandHandler("list_bookmark", list_bookmark, pass_args=True))
+    dispatcher.add_handler(CommandHandler("remove_bookmark", remove_bookmark, pass_args=True))
 
 
 
--- storage_controller.py
+++ storage_controller.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 FILE_NAME="bookmarks-storage.json"
 """
@@ -27,6 +28,20 @@ def read():
         print('[DEBUG-storage_controller.read] - data in storage', data)
     return data
 
+
+def write(data_new):
+    with open(FILE_NAME, 'w') as out_json:
+        json.dump(data_new, out_json)
+        print('[DEBUG-storage_controller.write] - data to storage', data_new)
+
+
+def read_by_chat_id(id):
+    with open(FILE_NAME, 'r') as infile:
+        data = json.load(infile)
+    print('[DEBUG-storage_controller.read_by_chat_id] - DATA READ BY CHAT ID: ', data[id])
+    return data[id]
+
+
 def check_if_id_exists(id):
     data_l = read()
     print(type(data_l))
@@ -38,15 +53,72 @@ def check_if_id_exists(id):
         print('[DEBUG-storage_controller.check_if_id_exists] - ID NOT EXISTS')
         return False
 
-
-def read_by_chat_id(id):
-    with open(FILE_NAME, 'r') as infile:
-        data = json.load(infile)
-    return data[id]
-
-
-def write(data):
-    with open(FILE_NAME, 'a') as outfile:
-        json.dump(data, outfile)
+def check_if_topic_exists(id, topic):
+    data_storage = read()
+    if topic in data_storage[str(id)]:
+        print('[DEBUG-storage_controller.check_if_topic_exists] - TOPIC {} ALREADY EXISTS'.format(topic))
+        return True
+    else:
+        print('[DEBUG-storage_controller.check_if_topic_exists] - TOPIC {} NOT EXISTS'.format(topic))
+        return False
 
 
+############## ADD
+def add_chat_id_to_storage(id):
+    data_new= {"{}".format(id):{}}
+    print('[DEBUG-storage_controller.add_chat_id_to_storage] - new id to storage', data_new)
+    with open(FILE_NAME, 'r') as in_json:
+        data_in = json.load(in_json)
+        print('[DEBUG-storage_controller.add_chat_id_to_storage] - data already in storage', data_in)
+    data_in[id] = {}
+    print('[DEBUG-storage_controller.add_chat_id_to_storage] - data updated to storage', data_in)
+    with open(FILE_NAME, 'w') as out_json:
+        json.dump(data_in, out_json)
+        print('[DEBUG-storage_controller.add_chat_id_to_storage] - data to storage', data_new)
+
+def add_topic_by_id_to_storage(id, topic):
+    data_in=read()
+    data_in[id][topic]=[]
+    print('[DEBUG-storage_controller.add_topic_by_id_to_storage] - data updated to storage', data_in)
+    write(data_in)
+
+def add_url_by_id_and_topic_to_storage(id, topic, url):
+    data_in=read() 
+    data_in[id][topic].append(url)
+    print('[DEBUG-storage_controller.add_topic_by_id_to_storage] - data updated to storage', data_in[id][topic])
+    write(data_in)
+
+
+############# LIST
+def list_topics_by_id(id):
+    data_in=read()
+    print('[DEBUG-storage_controller.list_bookmarks_by_id_and_topic] - list ', data_in[id])
+    return data_in[id]
+
+
+def list_bookmarks_by_id_and_topic(id, topic):
+    data_in=read()
+    print('[DEBUG-storage_controller.list_bookmarks_by_id_and_topic] - list ', data_in[id][topic])
+    return data_in[id][topic]
+
+
+########### REMOVE
+def remove_topic_by_id(id, topic):
+    data_in=read()
+    print('[DEBUG-storage_controller.remove_topic_by_id] - list ', data_in[id])
+    res = data_in.pop(topic, None)
+    if res != None:
+        print('[DEBUG-storage_controller.remove_topic_by_id] - deleted  from storage ', res)
+    else:
+        print('[DEBUG-storage_controller.remove_topic_by_id] - nothing to delete ')
+    return data_in[id]
+
+def remove_url_by_id_and_topic(id, topic, url):
+    data_in=read()
+    print('[DEBUG-storage_controller.remove_url_by_id_and_topic_] - list ', data_in[id][topic])
+    try:
+        data_in[id][topic].remove(url)
+        print('[DEBUG-storage_controller.remove_url_by_id_and_topic_] - deleted  from storage topic-{}:url-{} '.format(topic, url))
+        write(data_in)
+    except ValueError as error:
+        print("[ERROR] - ELEMNT DOESNOT EXISTS", error)
